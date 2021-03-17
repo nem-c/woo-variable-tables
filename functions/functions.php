34e016023b4a3fa4a23c74b6603199e312ab0fa0
Nemanja Cimbaljevic <zion.trooper@gmail.com>
2021-03-17 22:59:59 +0100

    functions CS fix

--- functions/functions.php
+++ functions/functions.php
@@ -1,1 +1,20 @@
 <?php
+/**
+ * Main file for additional functions
+ *
+ * @link       https://cimba.blog/
+ * @since      0.9.0
+ *
+ * @package    RFD\Woo_Variable_Tables
+ */
+
+if ( false === function_exists( 'rfd_woo_variable_tables_version' ) ) {
+	/**
+	 * Get plugin version
+	 *
+	 * @return string
+	 */
+	function rfd_woo_variable_tables_version(): string {
+		return RFD_WOO_VARIABLE_TABLES_VERSION;
+	}
+}
